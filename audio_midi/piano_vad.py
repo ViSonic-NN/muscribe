b8a5b8040777ce058853d68b10a6fd20c99d80ab
Yifan Zhao <yifanz16@illinois.edu>
2023-09-08 21:06:23 -0500

    Reformatted files in repo

--- audio_midi/piano_vad.py
+++ audio_midi/piano_vad.py
@@ -1,13 +1,16 @@
-import numpy as np
-
-
-def note_detection_with_onset_offset_regress(frame_output, onset_output, 
-    onset_shift_output, offset_output, offset_shift_output, velocity_output,
-    frame_threshold):
+def note_detection_with_onset_offset_regress(
+    frame_output,
+    onset_output,
+    onset_shift_output,
+    offset_output,
+    offset_shift_output,
+    velocity_output,
+    frame_threshold,
+):
     """Process prediction matrices to note events information.
     First, detect onsets with onset outputs. Then, detect offsets
     with frame and offset outputs.
-    
+
     Args:
       frame_output: (frames_num,)
       onset_output: (frames_num,)
@@ -16,11 +19,11 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
       offset_shift_output: (frames_num,)
       velocity_output: (frames_num,)
       frame_threshold: float
-    Returns: 
-      output_tuples: list of [bgn, fin, onset_shift, offset_shift, normalized_velocity], 
+    Returns:
+      output_tuples: list of [bgn, fin, onset_shift, offset_shift, normalized_velocity],
       e.g., [
-        [1821, 1909, 0.47498, 0.3048533, 0.72119445], 
-        [1909, 1947, 0.30730522, -0.45764327, 0.64200014], 
+        [1821, 1909, 0.47498, 0.3048533, 0.72119445],
+        [1909, 1947, 0.30730522, -0.45764327, 0.64200014],
         ...]
     """
     output_tuples = []
@@ -32,11 +35,12 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
         if onset_output[i] == 1:
             """Onset detected"""
             if bgn:
-                """Consecutive onsets. E.g., pedal is not released, but two 
+                """Consecutive onsets. E.g., pedal is not released, but two
                 consecutive notes being played."""
                 fin = max(i - 1, 0)
-                output_tuples.append([bgn, fin, onset_shift_output[bgn], 
-                    0, velocity_output[bgn]])
+                output_tuples.append(
+                    [bgn, fin, onset_shift_output[bgn], 0, velocity_output[bgn]]
+                )
                 frame_disappear, offset_occur = None, None
             bgn = i
 
@@ -57,15 +61,29 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
                 else:
                     """bgn --- offset_occur --------- frame_disappear"""
                     fin = frame_disappear
-                output_tuples.append([bgn, fin, onset_shift_output[bgn], 
-                    offset_shift_output[fin], velocity_output[bgn]])
+                output_tuples.append(
+                    [
+                        bgn,
+                        fin,
+                        onset_shift_output[bgn],
+                        offset_shift_output[fin],
+                        velocity_output[bgn],
+                    ]
+                )
                 bgn, frame_disappear, offset_occur = None, None, None
 
             if bgn and (i - bgn >= 600 or i == onset_output.shape[0] - 1):
                 """Offset not detected"""
                 fin = i
-                output_tuples.append([bgn, fin, onset_shift_output[bgn], 
-                    offset_shift_output[fin], velocity_output[bgn]])
+                output_tuples.append(
+                    [
+                        bgn,
+                        fin,
+                        onset_shift_output[bgn],
+                        offset_shift_output[fin],
+                        velocity_output[bgn],
+                    ]
+                )
                 bgn, frame_disappear, offset_occur = None, None, None
 
     # Sort pairs by onsets
@@ -74,20 +92,21 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
     return output_tuples
 
 
-def pedal_detection_with_onset_offset_regress(frame_output, offset_output, 
-    offset_shift_output, frame_threshold):
+def pedal_detection_with_onset_offset_regress(
+    frame_output, offset_output, offset_shift_output, frame_threshold
+):
     """Process prediction array to pedal events information.
-    
+
     Args:
       frame_output: (frames_num,)
       offset_output: (frames_num,)
       offset_shift_output: (frames_num,)
       frame_threshold: float
-    Returns: 
-      output_tuples: list of [bgn, fin, onset_shift, offset_shift], 
+    Returns:
+      output_tuples: list of [bgn, fin, onset_shift, offset_shift],
       e.g., [
-        [1821, 1909, 0.4749851, 0.3048533], 
-        [1909, 1947, 0.30730522, -0.45764327], 
+        [1821, 1909, 0.4749851, 0.3048533],
+        [1909, 1947, 0.30730522, -0.45764327],
         ...]
     """
     output_tuples = []
@@ -115,13 +134,13 @@ def pedal_detection_with_onset_offset_regress(frame_output, offset_output,
 
             if offset_occur:
                 fin = offset_occur
-                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                output_tuples.append([bgn, fin, 0.0, offset_shift_output[fin]])
                 bgn, frame_disappear, offset_occur = None, None, None
 
             if frame_disappear and i - frame_disappear >= 10:
                 """offset not detected but frame disappear"""
                 fin = frame_disappear
-                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                output_tuples.append([bgn, fin, 0.0, offset_shift_output[fin]])
                 bgn, frame_disappear, offset_occur = None, None, None
 
     # Sort pairs by onsets
--- example.py
+++ example.py
@@ -1,9 +1,9 @@
-import os
 import argparse
-import torch
 import time
 
-from piano_transcription_inference import PianoTranscription, sample_rate, load_audio
+import torch
+
+from audio_midi import PianoTranscription, load_audio, sample_rate
 
 
 def inference(args):
@@ -18,8 +18,8 @@ def inference(args):
     # Arugments & parameters
     audio_path = args.audio_path
     output_midi_path = args.output_midi_path
-    device = 'cuda' if args.cuda and torch.cuda.is_available() else 'cpu'
- 
+    device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+
     # Load audio
     (audio, _) = load_audio(audio_path, sr=sample_rate, mono=True)
 
@@ -31,16 +31,15 @@ def inference(args):
 
     # Transcribe and write out to MIDI file
     transcribe_time = time.time()
-    transcribed_dict = transcriptor.transcribe(audio, output_midi_path)
-    print('Transcribe time: {:.3f} s'.format(time.time() - transcribe_time))
+    transcriptor.transcribe(audio, output_midi_path)
+    print("Transcribe time: {:.3f} s".format(time.time() - transcribe_time))
 
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--audio_path', type=str, required=True)
-    parser.add_argument('--output_midi_path', type=str, required=True)
-    parser.add_argument('--cuda', action='store_true', default=False)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--audio_path", type=str, required=True)
+    parser.add_argument("--output_midi_path", type=str, required=True)
+    parser.add_argument("--cuda", action="store_true", default=False)
 
     args = parser.parse_args()
     inference(args)
